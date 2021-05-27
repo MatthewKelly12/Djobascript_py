c46099642172363c7f7f669ca64bdcdf07f55905
Matthew Kelly <mkonabike@gmail.com>
2021-05-27 18:35:04 -0500

    added job count to job_search view and html template

--- djoba/views/get_jobs.py
+++ djoba/views/get_jobs.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from djoba.models import Job
+from djoba.models import JobSearch
 from django.contrib.auth.models import User
 
 def get_jobs(request):
 	jobs = Job.objects.filter(user=request.user.id)
+	search_jobs = []
+	# searches = JobSearch.objects.filter(id=jobs.job_search_id)
+	# for job in jobs:
+	# 	if
+	# search = JobSearch.objects.filter(id=pk)
+	# job_count = Job.objects.filter(job_search_id=pk).count()
+	# print(job_count)
 	template = 'djoba/jobs.html'
 
 	return render(request, template, {'jobs': jobs})
--- djoba/views/job_search.py
+++ djoba/views/job_search.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from djoba.models import JobSearch
+from djoba.models import Job
 from django.contrib.auth.models import User
 
 def job_search(request,pk):
 	search = JobSearch.objects.filter(id=pk)
+	job_count = Job.objects.filter(job_search_id=pk).count()
+	print(job_count)
 	template = 'djoba/job_search.html'
 
-	return render(request,template,{'search':search})
+	return render(request,template,{'search':search,'job_count':job_count})
